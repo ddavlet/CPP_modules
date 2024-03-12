04982e728b2f8515ae8b733e7313b7f9e86dc678
Dilshod Davletyarov <dilshod95@gmail.com>
2024-03-12 19:28:01 +0100

    merge

---
+++ CPP00-05/ex02/Account.cpp
@@ -0,0 +1,60 @@
+#include "Account.hpp"
+
+Account::Account(int initial_deposit)
+{
+
+}
+
+Account::~Account(void)
+{
+}
+
+int	Account::getNbAccounts(void)
+{
+
+}
+
+int	Account::getTotalAmount(void)
+{
+
+}
+
+int	Account::getNbDeposits(void)
+{
+
+}
+
+int	Account::getNbWithdrawals(void)
+{
+
+}
+
+void	Account::displayAccountsInfos(void)
+{
+
+}
+
+void	Account::makeDeposit(int deposit)
+{
+
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+
+}
+
+int		Account::checkAmount(void) const
+{
+
+}
+
+void	Account::displayStatus(void) const
+{
+
+}
+
+void	Account::_displayTimestamp(void)
+{
+
+}
